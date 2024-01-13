7f22368e9fa839ff1fdb6df7abd98557c2597c3f
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-01-13 15:01:06 +0300

    rework func.py

--- func.py
+++ func.py
@@ -12,13 +12,23 @@ def load_operations() -> list:
         return data_list
 
 
-def sort_data() -> list:
+def is_operations_done(i) -> list:
+    """
+    Возвращает список по ключу "Executed"
+    :return: list
+    """
+    operations_done = []
+    for n in i:
+        if n.get("state") == "EXECUTED":
+            operations_done.append(n)
+    return operations_done
+
+
+def sort_data(i) -> list:
     """
     Возвращает отсортированный по дате список операций
     :return: list_sort_data
     """
-    with open("operations.json", "r", encoding="utf-8") as file:
-        data_list = json.load(file)
-        format_data = "%Y-%m-%dT%H:%M:%S.%f"
-        list_sorted_data = sorted(data_list, key=lambda x: datetime.strptime(x['date'], format_data))
-        return list_sorted_data
+    format_data = "%Y-%m-%dT%H:%M:%S.%f"
+    list_sorted_data = sorted(i, key=lambda x: datetime.strptime(x['date'], format_data))
+    return list_sorted_data
