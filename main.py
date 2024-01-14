6595b588112ad2f878e395dcad645ec92f0d107c
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-01-14 14:48:20 +0300

    rework main.py add utils.py

--- main.py
+++ main.py
@@ -1,77 +1,14 @@
-from func import *
-from datetime import datetime
-
-five_last_ex = five_last(sort_data(is_operations_done(load_operations())))
+from utils import *
 
 
-def shadow_to(accaunt):
-    """
-    Функция возвращает замаскuрованный список счетов
-    :return:['Счет **0000',...]
-    """
-    disguise_list = []
-    for item in accaunt:
-        accaunt_sort = item['to']
-        accaunt_sort_shadow = f"{accaunt_sort[:4]} **{accaunt_sort[-4:]}"
-        disguise_list.append(accaunt_sort_shadow)
-
-    return disguise_list
-
-
-def date_format(n):
-    """
-    Функция форматирования даты и возврата списка дат операций
-    :return: list ['%d.%m.%Y', '%d.%m.%Y'...]
-    """
-    format_date_list = []
-    for i in n:
-        date = datetime.strptime(i.get("date"), "%Y-%m-%dT%H:%M:%S.%f").strftime('%d.%m.%Y')
-        format_date_list.append(date)
-    return format_date_list
-
-
-def get_description(n) -> list:
-    """
-    Функция формиррует список наименования операции
-    :return: list ['','',''...]
-    """
-    description_list = []
-    for item in n:
-        description_list.append(item.get("description"))
-    return description_list
-
-
-def shadow_from(n) -> list:
-    """
-    Функция формирует список замаскированных счетов отправитея в случае их отсутствия
-    выводит "Открытие вклада"
-    :return:
-    """
-    shadow_list_from = []
-    for item in n:
-        if item.get("from"):
-            shadow = item["from"]
-            if "a" in shadow:
-                shadow = f"{shadow[:-16]}** **** {shadow[-4:]}"
-            else:
-                shadow = f"{shadow[:4]} **{shadow[-4:]}"
-            shadow_list_from.append(shadow)
-        else:
-            shadow_list_from.append('Открытие вклада')
-    return shadow_list_from
-
-
-def get_amount(n) -> list:
-    """
-    Функция возвращает список размера операции и валюту
-    :return: ['31234.23 USD', '31234.23 руб',...]
-    """
-    amount_list = []
-    for item in n:
-        amount = f'{item["operationAmount"]["amount"]} {item["operationAmount"]["currency"]["name"]}'
-        amount_list.append(amount)
-    return amount_list
+def main():
+    x = 0
+    while x <= 4:
+        print(f"{date_format(five_last_ex)[x]} {get_description(five_last_ex)[x]}\n"
+              f"{shadow_from(five_last_ex)[x]} -> {shadow_to(five_last_ex)[x]}"
+              f"\n{get_amount(five_last_ex)[x]}\n")
+        x += 1
 
 
 if __name__ == '__main__':
-    print(get_amount(five_last_ex))
+    main()
